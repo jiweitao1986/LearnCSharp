5e0c02e344d77d60a9c91c84b06a8cc50bc3e0f6
jiweitao1986 <jiweitao1986@gmail.com>
2015-06-24 17:54:42 +0800

    新增TreeList测试

--- Src/DevExpress/DevExpress/TestForm.Designer.cs
+++ Src/DevExpress/DevExpress/TestForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DevExpress
 {
-    partial class Form1
+    partial class TestForm
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.treeList1 = new DevExpress.XtraTreeList.TreeList();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // treeList1
+            // 
+            this.treeList1.Location = new System.Drawing.Point(62, 70);
+            this.treeList1.Name = "treeList1";
+            this.treeList1.Size = new System.Drawing.Size(400, 200);
+            this.treeList1.TabIndex = 0;
+            // 
+            // TestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(575, 401);
+            this.Controls.Add(this.treeList1);
+            this.Name = "TestForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TestForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private XtraTreeList.TreeList treeList1;
     }
 }
 
---
+++ Src/DevExpress/DevExpress/TestForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DevExpress.XtraTreeList.Nodes;
+
+namespace DevExpress
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        public void TestForm_Load(object sender, EventArgs e)
+        {
+            //TreeListNode node = new TreeListNode();
+            treeList1.AppendNode(new object[] { "根节点", 1 }, null);
+        }
+    }
+}
