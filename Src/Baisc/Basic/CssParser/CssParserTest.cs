ba77a7e3a8bc18cad52f516aa6d142c266b671dc
jiweitao1986 <jiweitao1986@gmail.com>
2015-07-09 18:09:16 +0800

    新增CSS属性值解析类，暂未实现。

---
+++ Src/Baisc/Basic/CssParser/CssParserTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Basic.CssParser
+{
+    class CssParserTest
+    {
+        public static void Run()
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Border()
+        {
+            string border1 = "1px solid #f00;";
+        }
+    }
+}
