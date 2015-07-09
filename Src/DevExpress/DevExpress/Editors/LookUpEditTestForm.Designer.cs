92fa43e2548ff7762123c97cf88fefdfc244383b
jiweitao1986 <jiweitao1986@gmail.com>
2015-07-09 18:13:21 +0800

    新增DevExpress LookupEdit测试表单

---
+++ Src/DevExpress/DevExpress/Editors/LookUpEditTestForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DevExpress.Editors
+{
+    partial class LookUpEditTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleLookUpEdit = new DevExpress.XtraEditors.LookUpEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpleLookUpEdit.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleLookUpEdit
+            // 
+            this.simpleLookUpEdit.Location = new System.Drawing.Point(32, 24);
+            this.simpleLookUpEdit.Name = "simpleLookUpEdit";
+            this.simpleLookUpEdit.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.simpleLookUpEdit.Properties.Columns.AddRange(new DevExpress.XtraEditors.Controls.LookUpColumnInfo[] {
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("ID", "部门ID"),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("Name", "部门名称")});
+            this.simpleLookUpEdit.Size = new System.Drawing.Size(260, 21);
+            this.simpleLookUpEdit.TabIndex = 0;
+            // 
+            // LookUpEditTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 262);
+            this.Controls.Add(this.simpleLookUpEdit);
+            this.Name = "LookUpEditTestForm";
+            this.Text = "LookUpEditTestForm";
+            this.Load += new System.EventHandler(this.LookUpEditTestForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.simpleLookUpEdit.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private XtraEditors.LookUpEdit simpleLookUpEdit;
+    }
+}
---
+++ Src/DevExpress/DevExpress/Editors/LookUpEditTestForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevExpress.Editors
+{
+    public partial class LookUpEditTestForm : Form
+    {
+        public LookUpEditTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LookUpEditTestForm_Load(object sender, EventArgs e)
+        {
+            this.simpleLookUpEdit.EditValue = "gsp";
+            this.simpleLookUpEdit.Properties.DisplayMember = "ID";
+            this.simpleLookUpEdit.Properties.ValueMember = "Name";
+            this.simpleLookUpEdit.Properties.DataSource = this.GetDepartments();
+
+        }
+
+        private DataTable GetDepartments()
+        {
+            DataTable deptDt = new DataTable();
+            DataColumn idDc = new DataColumn("ID", Type.GetType("System.String"));
+            DataColumn nameDc = new DataColumn("Name", Type.GetType("System.String"));
+            deptDt.Columns.Add(idDc);
+            deptDt.Columns.Add(nameDc);
+
+            //Rows
+            DataRow gspDr = deptDt.NewRow();
+            gspDr["ID"] = "gsp";
+            gspDr["Name"] = "平台与技术部";
+            deptDt.Rows.Add(gspDr);
+            return deptDt;
+        }
+    }
+}
