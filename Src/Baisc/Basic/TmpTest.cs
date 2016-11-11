b89ea0391a38e4d33b9c0939220e0c7182f7fb0e
jiweitao1986 <jiweitao1986@163.com>
2016-11-11 18:18:23 +0800

    TmpTest修改，无重要变化。

--- Src/Baisc/Basic/TmpTest.cs
+++ Src/Baisc/Basic/TmpTest.cs
@@ -15,6 +15,21 @@ namespace LearnCSharp.Basic
     {
         public static void Run()
         {
+
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 
